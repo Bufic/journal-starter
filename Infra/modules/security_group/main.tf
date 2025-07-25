dcf25a9b91558e78cf2bfd87bbbbe1d0f21164d6
bufic <fubaraemmanueljnr@gmail.com>
2025-07-25 15:45:44 +0100

    deployment to AWS enviroment

--- Infra/modules/security_group/main.tf
+++ Infra/modules/security_group/main.tf
@@ -3,6 +3,27 @@ resource "aws_security_group" "fastapi_sg" {
   vpc_id = var.vpc_id
 
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Restrict to your IP/GitHub IPs in production
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
@@ -15,11 +36,23 @@ resource "aws_security_group" "fastapi_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "fastapi-sg"
+  }
+
 }
 
 resource "aws_security_group" "postgres_sg" {
   name   = "postgres-sg"
   vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Restrict to your IP/GitHub IPs in production
+  }
 
   ingress {
     from_port       = 5432
@@ -33,5 +66,9 @@ resource "aws_security_group" "postgres_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "postgres-sg"
   }
 }
